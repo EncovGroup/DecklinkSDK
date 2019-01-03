8628479277ccce9515e3937d71f2ee773e29903a
Simone Gasparini <simone.gasparini@gmail.com>
2019-01-03 11:58:52 +0100

    [cmake] fix linux sources

--- linuxSources.cmake
+++ linuxSources.cmake
@@ -3,4 +3,10 @@ set(SDK_SOURCES
 
 set(SDK_HEADERS
         Linux/include/DeckLinkAPI.h
+        Linux/include/LinuxCOM.h
+        Linux/include/DeckLinkAPITypes.h
+        Linux/include/DeckLinkAPIModes.h
+        Linux/include/DeckLinkAPIDiscovery.h
+        Linux/include/DeckLinkAPIConfiguration.h
+        Linux/include/DeckLinkAPIDeckControl.h
         Linux/include/DeckLinkAPIVersion.h)
