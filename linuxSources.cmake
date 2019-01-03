1d117ab63f7672d9e441112d296e0b797d5fe3d3
Simone Gasparini <simone.gasparini@gmail.com>
2019-01-03 11:00:59 +0100

    [cmake] first version osx only

---
+++ linuxSources.cmake
@@ -0,0 +1,6 @@
+set(SDK_SOURCES
+        Linux/include/DeckLinkAPIDispatch.cpp)
+
+set(SDK_HEADERS
+        Linux/include/DeckLinkAPI.h
+        Linux/include/DeckLinkAPIVersion.h)
---
+++ macosxSources.cmake
@@ -0,0 +1,12 @@
+set(SDK_SOURCES
+        Mac/include/DeckLinkAPIDispatch.cpp)
+
+set(SDK_HEADERS
+        Mac/include/DeckLinkAPI.h
+        Mac/include/DeckLinkAPITypes.h
+        Mac/include/DeckLinkAPIModes.h
+        Mac/include/DeckLinkAPIDiscovery.h
+        Mac/include/DeckLinkAPIConfiguration.h
+        Mac/include/DeckLinkAPIDeckControl.h
+        Mac/include/DeckLinkAPIStreaming.h
+        Mac/include/DeckLinkAPIVersion.h)
